698dd2b6f29f49aecc6eb95cb32e05eb69e29774
dropecosta <dropecosta@gmail.com>
2018-05-08 18:59:54 -0300

    updating files

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
+import Grid from '../template/grid'
 
 export default props => (
     <div role='form' className='todoForm'>
-        <div className='col-xs12 col-sm-9 col-md10'>
+        <Grid cols='12 9 10'>
             <input id='description' className='form-control' placeholder='adicione uma tarefa'></input>
-        </div>
+        </Grid>
 
-        <div className='col-xs12 col-sm-3 col-md2'>
+        <Grid cols='12 3 2'>
             <button className='btn btn-primary'>
                 <i className='fa fa-plus'></i>
             </button>
-        </div>
+        </Grid>
     </div>
 )
