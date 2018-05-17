81a21baa69873dd80324fec5458ebee2a8fcc452
dropecosta <dropecosta@gmail.com>
2018-05-17 13:35:33 -0300

    adding search and clear search

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -9,10 +9,15 @@ export default props => {
 
         return list.map(todo => (
             <tr key={todo._id}>
-                <td>{todo.description}</td>
+                <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
-                    <IconButton style='danger' icon='trash-o'
-                        onClick={() => props.handleRemove(todo)}></IconButton></td>
+                    <IconButton style='success' hide={todo.done} icon='check'
+                        onClick={() => props.handleMarkAsDone(todo)}></IconButton>
+                    <IconButton style='warning' hide={!todo.done} icon='undo'
+                        onClick={() => props.handleMarkAsPending(todo)}></IconButton>
+                    <IconButton style='danger' hide={!todo.done} icon='trash-o'
+                        onClick={() => props.handleRemove(todo)}></IconButton>
+                </td>
             </tr>
         ))
     }
@@ -21,8 +26,8 @@ export default props => {
         <table className='table'>
             <thead>
                 <tr>
-                    <th>Descrição</th> 
-                    <th>Ações</th> 
+                    <th className='tableActions'>Descrição</th> 
+                    <th className='tableActions'>Ações</th> 
                 </tr>
             </thead>
             <tbody>
